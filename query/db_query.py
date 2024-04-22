27f1c053c552e49e928ed59696e12cf0b622e37d
Steve Pousty <steve.pousty@gmail.com>
2024-04-22 07:29:43 -0700

    changes

--- query/db_query.py
+++ query/db_query.py
@@ -1,18 +1,19 @@
-from qdrant_client import QdrantClient
-from qdrant_client.models import Distance, VectorParams
-from qdrant_client.models import models
+import psycopg
+from pgvector.psycopg import register_vector
 
-
+# @todo this whole class needs to be rewritten
 class DBQuery:
 
-    def __init__(self, vector_size: int, collection_name: str):
+    def __init__(self):
 
-        self.client = QdrantClient(host="localhost", port=6333)
+        self.connect_string = f"host=localhost user=postgres password='letmein' dbname=geoimage"
+        self.conn = psycopg.connect(self.connect_string, autocommit=True)
 
-        self.vector_size = vector_size
-        self.collection_name = collection_name
-
+    # just a normal vector query
     def query_vector(self, vector):
+        with self.conn.cursor() as cur:
+            cur.execute(SQL)
+
         vector = vector[0]
         search_result = self.client.search(
             collection_name=self.collection_name,
@@ -21,6 +22,8 @@ class DBQuery:
         )
         return search_result
 
+    # this one includes spatial
+    # for the workshop we will hardcode the country to use
     def query_geo_vector(self, vector):
         vector = vector[0]
         search_result = self.client.search(
