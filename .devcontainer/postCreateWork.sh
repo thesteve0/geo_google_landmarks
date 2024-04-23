615b57682f88ca5d5812cfc254f0e254ade0a72b
Steve Pousty <steve.pousty@gmail.com>
2024-04-23 07:34:04 -0700

    working on devcontainer setup

--- .devcontainer/postCreateWork.sh
+++ .devcontainer/postCreateWork.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-wget dbbackup
-wget testimages
+wget -qb https://github.com/thesteve0/geo_google_landmarks/releases/download/v0.1.1/geo_image_dump_sql.gz
+wget -qb https://github.com/thesteve0/geo_google_landmarks/releases/download/v0.1/image_query_files.tar.gz
 
 tar -xvf image_query_files.tar.gz
 
